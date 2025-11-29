4bf122fe664a03e066282272ba2c1699d4116a4d
Max Harris <max@Maxs-MacBook-Air.local>
2025-11-28 21:46:07 -0800

    fixes background scene

--- src/background/WebglScene.js
+++ src/background/WebglScene.js
@@ -1,5 +1,12 @@
 import * as THREE from 'three';
 import { gsap } from 'gsap';
+import alphaTextureImg from '../../assets/images/background/texture-alpha.png';
+import wireframeTextureImg from '../../assets/images/background/texture-wireframe.png';
+
+// Nobody likes your console logs mr. THREE.bs
+const consoleLog = console.log; // eslint-disable-line no-console
+// eslint-disable-next-line no-console
+console.log = (first, ...args) => (first !== 'THREE.WebGLRenderer') && consoleLog.call(console, first, ...args);
 
 const COLOR_GRADIENTS = {
 	green: ['#c0f4fe', '#fff3df'],
@@ -11,22 +18,42 @@ export default class WebglScene {
 		this.width = window.innerWidth;
 		this.height = window.innerHeight;
 
+		// scene set up
 		this.scene = new THREE.Scene();
 		this.camera = new THREE.PerspectiveCamera(75, this.width / this.height);
 		this.camera.far = 10000;
 		this.camera.position.z = 36;
+		this.controls = null;
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-		this.renderer.setClearColor(0x000000, 0);
 		this.renderFlag = false;
 
+		// canvas background to transparent
+		this.renderer.setClearColor(0x000000, 0);
+
+		// dom element
 		this.container = container;
+
+		// geometries
+		this.nbVerticesPlane = 0;
+		this.planeGeo1 = null;
+		this.planeGeo2 = null;
+		this.planeGeo3 = null;
+
+		// materials
+		this.material1 = null;
+		this.material2 = null;
+		this.material3 = null;
+
+		// geometries var
 		this.nbSegments = 64;
 		this.lengthPlane = 90;
 		this.widthPlane = 12;
 		this.widthPlaneMini = 8;
 
+		// groups
 		this.globalGroup = new THREE.Group();
 
+		// animation vars
 		this.frame = 0;
 		this.flagCount = 0;
 		this.tWave1 = 0;
@@ -38,30 +65,31 @@ export default class WebglScene {
 		this.tWave3 = 0;
 		this.tY3 = 0;
 
+		// mousemove
 		this.mousemoveX = 0;
 		this.mousemoveY = 0;
 		this.mouse = { x: 0, y: 0 };
 
+		// scroll
 		this.scrollY = 0;
 		this.y = {
-			val1: 0,
-			val2: 0,
-			val3: 0,
-			val4: 0,
-			anim1: 0.65,
-			anim2: 0.65,
-			anim3: 0.65,
+			val1: 0, val2: 0, val3: 0, val4: 0, anim1: 0.65, anim2: 0.65, anim3: 0.65,
 		};
 		this.finalY = { val1: 0, val2: 0, val3: 0, val4: 0 };
 		this.sauvScrollY = 0;
+
+		// show hide helix
 		this.helixActive = false;
 
+		// functions
 		this.resizeCanvas();
 		this.createLights();
 		this.createTextures();
 		this.createGeometries();
 		this.twistPlanes();
-		this.renderScene(window.devicePixelRatio || 1);
+
+		this.renderScene(1);
+
 		this.scene.add(this.globalGroup);
 	}
 
@@ -80,39 +108,53 @@ export default class WebglScene {
 	}
 
 	createTextures() {
-		const gradientGreen = this.createGradientTexture(...COLOR_GRADIENTS.green);
-		const gradientBlue = this.createGradientTexture(...COLOR_GRADIENTS.blue);
-		const alphaTexture = createAlphaTexture();
-		const wireframeTexture = createWireframeTexture();
+		// loader util
+		const textureLoader = new THREE.TextureLoader();
+		textureLoader.crossOrigin = '';
+		const textureAlpha = textureLoader.load(alphaTextureImg);
+		const wireframeTexture = textureLoader.load(wireframeTextureImg);
 
+		// green gradient texture
+		const gradientGreen = new THREE.Texture(this.createGradient(...COLOR_GRADIENTS.green));
+		gradientGreen.needsUpdate = true;
 		this.material1 = new THREE.MeshBasicMaterial({
 			map: gradientGreen,
 			side: THREE.DoubleSide,
 			transparent: true,
-			alphaMap: alphaTexture,
 		});
+		this.material1.alphaMap = textureAlpha;
+		this.material1.alphaMap.magFilter = THREE.NearestFilter;
 
+		// blue gradient texture
+		const gradientBlue = new THREE.Texture(this.createGradient(...COLOR_GRADIENTS.blue));
+		gradientBlue.needsUpdate = true;
 		this.material2 = new THREE.MeshBasicMaterial({
 			map: gradientBlue,
 			side: THREE.DoubleSide,
 			transparent: true,
-			alphaMap: alphaTexture,
 		});
+		this.material2.alphaMap = textureAlpha;
+		this.material2.alphaMap.magFilter = THREE.NearestFilter;
 
 		this.material3 = new THREE.MeshBasicMaterial({
 			color: 0x94d3fd,
 			side: THREE.DoubleSide,
 			transparent: true,
 			depthWrite: false,
-			alphaMap: wireframeTexture,
 		});
+		this.material3.alphaMap = wireframeTexture;
+		this.material3.alphaMap.magFilter = THREE.NearestFilter;
 	}
 
-	createGradientTexture(leftColor, rightColor) {
+	createGradient(leftColor, rightColor) {
 		const canvas = document.createElement('canvas');
 		canvas.width = 512;
 		canvas.height = 512;
+
+		// get context
 		const context = canvas.getContext('2d');
+
+		// draw gradient
 		context.rect(0, 0, 512, 512);
 		const gradient = context.createLinearGradient(0, 0, 512, 512);
 		gradient.addColorStop(0, leftColor);
@@ -120,33 +162,31 @@ export default class WebglScene {
 		context.fillStyle = gradient;
 		context.fill();
 
-		const texture = new THREE.CanvasTexture(canvas);
-		texture.needsUpdate = true;
-		return texture;
+		return canvas;
 	}
 
 	createGeometries() {
-		// Verify materials exist before creating geometries
-		if (!this.material1 || !this.material2 || !this.material3) {
-			console.error('WebglScene: Materials must be created before geometries');
-			return;
-		}
-
-		// First geometry
-		this.planeGeo1 = new THREE.PlaneGeometry(this.widthPlane, this.lengthPlane, this.nbSegments, this.nbSegments);
+		// first geometry
+		this.planeGeo1 = new THREE.PlaneGeometry(
+			this.widthPlane, this.lengthPlane, this.nbSegments, this.nbSegments,
+		);
 		const plane1 = new THREE.Mesh(this.planeGeo1, this.material1);
 		plane1.rotation.y = Math.PI / 2;
 
-		// Second geometry
-		this.planeGeo2 = new THREE.PlaneGeometry(this.widthPlane, this.lengthPlane, this.nbSegments, this.nbSegments);
+		// second geometry
+		this.planeGeo2 = new THREE.PlaneGeometry(
+			this.widthPlane, this.lengthPlane, this.nbSegments, this.nbSegments,
+		);
 		const plane2 = new THREE.Mesh(this.planeGeo2, this.material2);
 		plane2.rotation.y = Math.PI / 2;
 
-		// Third geometry
-		this.planeGeo3 = new THREE.PlaneGeometry(this.widthPlaneMini, this.lengthPlane, this.nbSegments, this.nbSegments);
+		// third geometry
+		this.planeGeo3 = new THREE.PlaneGeometry(
+			this.widthPlaneMini, this.lengthPlane, this.nbSegments, this.nbSegments,
+		);
 		const plane3 = new THREE.Mesh(this.planeGeo3, this.material3);
 
-		// Number of vertices (using old Geometry API with vertices array)
+		// nb vertices
 		this.nbVerticesPlane = this.planeGeo1.vertices.length;
 
 		this.globalGroup.add(plane1);
@@ -156,16 +196,22 @@ export default class WebglScene {
 
 	twistPlanes() {
 		let flagCount = 0;
+		let x1 = 0;
+		let x2 = 0;
+		let x3 = 0;
+
 		let frame = 0;
 		let j = this.nbVerticesPlane;
+		let easing = 1;
+		let t = 0;
 
-		for (let i = 0; i < this.nbVerticesPlane; i += 1) {
-			let t = (1 / this.nbVerticesPlane) * j;
-			const easing = (t -= 1) * t * t + 1;
+		for (let i = 0; i < this.nbVerticesPlane; i++) {
+			t = (1 / this.nbVerticesPlane) * j;
+			easing = (((t -= 1) * t) * t) + 1;
 
-			const x1 = this.planeGeo1.vertices[i].x * easing + Math.sin(1.45 + frame / 8) * (-6 * easing);
-			const x2 = this.planeGeo2.vertices[i].x * easing + Math.sin(1.45 + frame / 8) * (8 * easing);
-			const x3 = this.planeGeo3.vertices[i].x * easing + Math.sin(0.88 + frame / -5) * (3 * easing);
+			x1 = (this.planeGeo1.vertices[i].x * easing) + (Math.sin(1.45 + (frame / 8)) * (-6 * easing));
+			x2 = (this.planeGeo2.vertices[i].x * easing) + (Math.sin(1.45 + (frame / 8)) * (8 * easing));
+			x3 = (this.planeGeo3.vertices[i].x * easing) + (Math.sin(0.88 + (frame / -5)) * (3 * easing));
 
 			this.planeGeo1.vertices[i].x = x1;
 			this.planeGeo2.vertices[i].x = x2;
@@ -178,11 +224,6 @@ export default class WebglScene {
 			flagCount += 1;
 			j -= 1;
 		}
-
-		// Mark vertices as needing update (old Geometry API)
-		this.planeGeo1.verticesNeedUpdate = true;
-		this.planeGeo2.verticesNeedUpdate = true;
-		this.planeGeo3.verticesNeedUpdate = true;
 	}
 
 	updateMouseMove(x, y) {
@@ -193,6 +234,8 @@ export default class WebglScene {
 	showHelix() {
 		gsap.killTweensOf(this.y);
 		this.renderFlag = true;
+
+		// Using modern GSAP API with original ease values (Power2.easeIn maps to power2.in in GSAP v3+)
 		gsap.to(this.y, { duration: 2, anim1: -0.45, ease: 'power2.in' });
 		gsap.to(this.y, { duration: 1.9, anim2: -0.45, ease: 'power2.in' });
 		gsap.to(this.y, { duration: 2.1, anim3: -0.45, ease: 'power2.in' });
@@ -200,10 +243,11 @@ export default class WebglScene {
 
 	hideHelix() {
 		gsap.killTweensOf(this.y);
+
+		gsap.to(this.y, { duration: 2, anim1: 0.65, ease: 'power2.out' });
+		gsap.to(this.y, { duration: 2, anim2: 0.65, ease: 'power2.out' });
 		gsap.to(this.y, {
 			duration: 2,
-			anim1: 0.65,
-			anim2: 0.65,
 			anim3: 0.65,
 			ease: 'power2.out',
 			onComplete: () => {
@@ -212,14 +256,17 @@ export default class WebglScene {
 		});
 	}
 
-	renderScene(pixelRatio) {
+	renderScene(myPixelRatio) {
+		const { width, height } = this;
 		this.renderer.sortObjects = true;
-		this.renderer.setPixelRatio(pixelRatio);
-		this.renderer.setSize(this.width, this.height);
+
+		this.renderer.setPixelRatio(myPixelRatio);
+		this.renderer.setSize(width, height);
 		this.container.appendChild(this.renderer.domElement);
 
-		// Render for the first time to don't show black canvas
+		// Render for the first time to dont show black canvas.
 		this.renderer.render(this.scene, this.camera);
+
 		this.render();
 	}
 
@@ -234,62 +281,66 @@ export default class WebglScene {
 	loopWaveAnimation() {
 		let j = 0;
 		let k = this.nbVerticesPlane;
+		let t = 0;
+		let easing = 0;
 		let twistVar = 0;
 
-		for (let i = 0; i < this.nbVerticesPlane; i += 1) {
-			let t = (1 / this.nbVerticesPlane) * k;
-			const easing = (t -= 1) * t * t + 1;
+		for (let i = 0; i < this.nbVerticesPlane; i++) {
+			t = (1 / this.nbVerticesPlane) * k;
+			easing = (((t -= 1) * t) * t) + 1;
 
-			this.tWave1 = Math.sin(this.frame / 400000 + i / 10) * (1.2 * easing);
+			this.tWave1 = (Math.sin((this.frame / 400000) + (i / 10)) * (1.2 * easing));
 			this.twist1 = Math.sin((this.flagCount + twistVar) / 45) * (-6 * easing) * (1 + this.mouse.y);
-			this.tY1 = (1.2 + Math.sin(j / 10) * -10) * (1 + this.mouse.x);
+			this.tY1 = (1.2 + (Math.sin((j / 10)) * -10)) * (1 + this.mouse.x);
 
-			this.tWave2 = Math.sin(this.frame / 400000 + i / 10) * (1.2 * easing);
+			this.tWave2 = (Math.sin((this.frame / 400000) + (i / 10)) * (1.2 * easing));
 			this.twist2 = Math.sin((this.flagCount + twistVar) / 45) * (4 * easing) * (1 + this.mouse.y);
-			this.tY2 = Math.sin(3.4 + j / 10) * -4 * (1 + this.mouse.y);
+			this.tY2 = ((Math.sin(3.4 + (j / 10)) * -4)) * (1 + this.mouse.y);
 
-			this.tWave3 = Math.sin(this.frame / 400000 + i / 10) * (3 * easing);
-			this.tY3 = (Math.sin(3.4 + j / -40) * 6 + 18 * easing) * (1 + this.mouse.x);
+			this.tWave3 = (Math.sin((this.frame / 400000) + (i / 10)) * (3 * easing));
+			this.tY3 = ((Math.sin(3.4 + (j / -40)) * 6) + (18 * easing)) * (1 + this.mouse.x);
 
-			// Update z coordinate directly on vertices (old Geometry API)
 			this.planeGeo1.vertices[i].z = (this.tWave1 + this.twist1 + this.tY1) * easing;
+			this.planeGeo1.verticesNeedUpdate = true;
+
 			this.planeGeo2.vertices[i].z = (this.tWave2 + this.twist2 + this.tY2) * easing;
+			this.planeGeo2.verticesNeedUpdate = true;
+
 			this.planeGeo3.vertices[i].z = (this.tWave3 + this.tY3) * easing;
+			this.planeGeo3.verticesNeedUpdate = true;
 
 			if (this.flagCount === this.nbSegments) {
 				j += 1;
 				this.flagCount = -1;
 			}
-
 			twistVar += 0.1;
 			this.flagCount += 1;
 			this.frame += 1;
 			k -= 1;
 		}
-
-		// Mark vertices as needing update (old Geometry API)
-		this.planeGeo1.verticesNeedUpdate = true;
-		this.planeGeo2.verticesNeedUpdate = true;
-		this.planeGeo3.verticesNeedUpdate = true;
 	}
 
 	mousemoveLoopAnimation() {
-		this.mouse.x = 0.035 * (this.mousemoveX / 7500 - this.mouse.x) + this.mouse.x;
-		this.mouse.y = 0.035 * (this.mousemoveY / 7500 - this.mouse.y) + this.mouse.y;
+		this.mouse.x = (0.035 * ((this.mousemoveX / 7500) - this.mouse.x)) + this.mouse.x;
+		this.mouse.y = (0.035 * ((this.mousemoveY / 7500) - this.mouse.y)) + this.mouse.y;
+
 		this.globalGroup.rotation.y = this.mouse.x / 10;
 	}
 
 	scrollLoopAnimation() {
 		const relativeValue = this.height * 0.65;
-		this.y.val1 = 0.1 * (this.scrollY / relativeValue - this.y.val1) + this.y.val1;
-		this.y.val2 = 0.1 * (this.scrollY / relativeValue - this.y.val2) + this.y.val2;
-		this.y.val3 = 0.1 * (this.scrollY / relativeValue - this.y.val3) + this.y.val3;
+		// plane mask animation
+		this.y.val1 = ((0.1 * (((this.scrollY / relativeValue) - this.y.val1))) + this.y.val1);
+		this.y.val2 = (0.1 * (((this.scrollY / relativeValue) - this.y.val2))) + this.y.val2;
+		this.y.val3 = (0.1 * (((this.scrollY / relativeValue) - this.y.val3))) + this.y.val3;
 
+		// adding show hide animation
 		this.finalY.val1 = this.y.val1 + this.y.anim1;
 		this.finalY.val2 = this.y.val2 + this.y.anim2;
 		this.finalY.val3 = this.y.val3 + this.y.anim3;
 
-		if (this.material1) {
+		// apply the values
+		if (this.material1 !== null) {
 			this.material1.map.offset.y = this.finalY.val1;
 			this.material2.map.offset.y = this.finalY.val2;
 			this.material3.alphaMap.offset.y = this.finalY.val3;
@@ -297,64 +348,25 @@ export default class WebglScene {
 	}
 
 	helixYmovement() {
+		// global group movement
 		this.y.val4 = this.scrollY / (this.height * 0.025);
+
 		this.globalGroup.position.y = this.y.val4;
 	}
 
-	render() {
+	render = () => {
 		if (this.renderFlag) {
 			this.renderer.render(this.scene, this.camera);
+
+			if (this.controls !== null) {
+				this.controls.update();
+			}
+
 			this.loopWaveAnimation();
 			this.scrollLoopAnimation();
 			this.mousemoveLoopAnimation();
 			this.helixYmovement();
 		}
-		requestAnimationFrame(() => this.render());
+		requestAnimationFrame(this.render);
 	}
-}
-
-function createAlphaTexture() {
-	const canvas = document.createElement('canvas');
-	canvas.width = 1024;
-	canvas.height = 1024;
-	const ctx = canvas.getContext('2d');
-	const gradient = ctx.createRadialGradient(512, 512, 0, 512, 512, 512);
-	gradient.addColorStop(0, 'rgba(255,255,255,1)');
-	gradient.addColorStop(1, 'rgba(255,255,255,0)');
-	ctx.fillStyle = gradient;
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	const texture = new THREE.CanvasTexture(canvas);
-	texture.needsUpdate = true;
-	texture.wrapS = THREE.ClampToEdgeWrapping;
-	texture.wrapT = THREE.ClampToEdgeWrapping;
-	return texture;
-}
-
-function createWireframeTexture() {
-	const canvas = document.createElement('canvas');
-	canvas.width = 512;
-	canvas.height = 512;
-	const ctx = canvas.getContext('2d');
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.strokeStyle = 'rgba(255,255,255,0.35)';
-	ctx.lineWidth = 2;
-	const step = 32;
-	for (let i = 0; i <= canvas.width; i += step) {
-		ctx.beginPath();
-		ctx.moveTo(i, 0);
-		ctx.lineTo(i, canvas.height);
-		ctx.stroke();
-	}
-	for (let j = 0; j <= canvas.height; j += step) {
-		ctx.beginPath();
-		ctx.moveTo(0, j);
-		ctx.lineTo(canvas.width, j);
-		ctx.stroke();
-	}
-	const texture = new THREE.CanvasTexture(canvas);
-	texture.needsUpdate = true;
-	texture.wrapS = THREE.RepeatWrapping;
-	texture.wrapT = THREE.RepeatWrapping;
-	texture.repeat.set(4, 4);
-	return texture;
 }
