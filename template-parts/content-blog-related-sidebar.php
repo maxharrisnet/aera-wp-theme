ba98c73a25486861b1554bccd66c1e06b0e9eaf1
Max Harris <max@Maxs-MacBook-Air.local>
2026-01-21 00:18:37 -0800

    much better blog

--- template-parts/content-blog-related-sidebar.php
+++ template-parts/content-blog-related-sidebar.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for blog related sidebar (Other Resources links).
  *
@@ -8,49 +9,52 @@
 defined('ABSPATH') || exit;
 ?>
 
-<div class="blog-related__sidebar">
-  <h3><?php esc_html_e('Other Resources', 'aera'); ?></h3>
-  <ul>
-    <li>
-      <a href="<?php echo esc_url(get_permalink(get_page_by_path('what-is-decision-intelligence')) ?: '#'); ?>">
-        <?php esc_html_e('What is Decision Intelligence?', 'aera'); ?>
-      </a>
-    </li>
-    <li>
-      <a href="<?php echo esc_url(get_post_type_archive_link('news') ?: '#'); ?>">
-        <?php esc_html_e('News', 'aera'); ?>
-      </a>
-    </li>
-    <li>
-      <a href="<?php echo esc_url(get_post_type_archive_link('press-release') ?: '#'); ?>">
-        <?php esc_html_e('Press Releases', 'aera'); ?>
-      </a>
-    </li>
-    <li>
-      <a href="<?php echo esc_url(get_post_type_archive_link('video') ?: '#'); ?>">
-        <?php esc_html_e('Videos', 'aera'); ?>
-      </a>
-    </li>
-    <li>
-      <a href="<?php echo esc_url(get_post_type_archive_link('whitepaper') ?: '#'); ?>">
-        <?php esc_html_e('Whitepapers', 'aera'); ?>
-      </a>
-    </li>
-    <li>
-      <a href="<?php echo esc_url(get_post_type_archive_link('blog') ?: '#'); ?>">
-        <?php esc_html_e('Blogs', 'aera'); ?>
-      </a>
-    </li>
-    <li>
-      <a href="<?php echo esc_url(get_post_type_archive_link('case-study') ?: '#'); ?>">
-        <?php esc_html_e('Case Studies', 'aera'); ?>
-      </a>
-    </li>
-    <li>
-      <a href="<?php echo esc_url(get_post_type_archive_link('podcast') ?: '#'); ?>">
-        <?php esc_html_e('Podcasts', 'aera'); ?>
-      </a>
-    </li>
-  </ul>
-</div>
+<div class="blog-related">
+  <div class="blog-related__container">
 
+
+    <h3><?php esc_html_e('Other Resources', 'aera'); ?></h3>
+    <ul>
+      <li>
+        <a href="<?php echo esc_url(get_permalink(get_page_by_path('what-is-decision-intelligence')) ?: '#'); ?>">
+          <?php esc_html_e('What is Decision Intelligence?', 'aera'); ?>
+        </a>
+      </li>
+      <li>
+        <a href="<?php echo esc_url(get_post_type_archive_link('news') ?: '#'); ?>">
+          <?php esc_html_e('News', 'aera'); ?>
+        </a>
+      </li>
+      <li>
+        <a href="<?php echo esc_url(get_post_type_archive_link('press-release') ?: '#'); ?>">
+          <?php esc_html_e('Press Releases', 'aera'); ?>
+        </a>
+      </li>
+      <li>
+        <a href="<?php echo esc_url(get_post_type_archive_link('video') ?: '#'); ?>">
+          <?php esc_html_e('Videos', 'aera'); ?>
+        </a>
+      </li>
+      <li>
+        <a href="<?php echo esc_url(get_post_type_archive_link('whitepaper') ?: '#'); ?>">
+          <?php esc_html_e('Whitepapers', 'aera'); ?>
+        </a>
+      </li>
+      <li>
+        <a href="<?php echo esc_url(get_post_type_archive_link('blog') ?: '#'); ?>">
+          <?php esc_html_e('Blogs', 'aera'); ?>
+        </a>
+      </li>
+      <li>
+        <a href="<?php echo esc_url(get_post_type_archive_link('case-study') ?: '#'); ?>">
+          <?php esc_html_e('Case Studies', 'aera'); ?>
+        </a>
+      </li>
+      <li>
+        <a href="<?php echo esc_url(get_post_type_archive_link('podcast') ?: '#'); ?>">
+          <?php esc_html_e('Podcasts', 'aera'); ?>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
