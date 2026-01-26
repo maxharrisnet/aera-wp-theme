275c240cc3c2ce01cd2e5530bbabcc9832088c82
Max Harris <max@Maxs-MacBook-Air.local>
2026-01-26 05:55:19 -0800

    adds hero component to default page template

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -14,65 +15,60 @@
 
 get_header();
 
-while ( have_posts() ) :
-	the_post();
+while (have_posts()) :
+  the_post();
 
-	// Get optional lead text from ACF field
-	$lead_text = function_exists('get_field') ? (string) get_field('page_lead') : '';
-	$lead_paragraphs = array_values(array_filter(array_map('trim', preg_split('/\r\n|\r|\n/', $lead_text ?? ''))));
+  // Get optional lead text from ACF field
+  $lead_text = function_exists('get_field') ? (string) get_field('page_lead') : '';
+  $lead_paragraphs = array_values(array_filter(array_map('trim', preg_split('/\r\n|\r|\n/', $lead_text ?? ''))));
 
-	// Get date (optional, can be hidden via ACF or use post date)
-	$show_date = function_exists('get_field') ? get_field('page_show_date') : true;
-	$date_value = get_the_date('c');
-	$display_date = get_the_date();
-	?>
+  // Get date (optional, can be hidden via ACF or use post date)
+  $show_date = function_exists('get_field') ? get_field('page_show_date') : true;
+  $date_value = get_the_date('c');
+  $display_date = get_the_date();
+?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class('template-page'); ?>>
-		<div class="template-page__container">
-			<div class="template-page__row">
-				<div class="template-page__col">
-					<header class="template-page__header">
-						<?php if ($show_date && $display_date) : ?>
-							<p class="template-page__date">
-								<time datetime="<?php echo esc_attr($date_value); ?>">
-									<?php echo esc_html($display_date); ?>
-								</time>
-							</p>
-						<?php endif; ?>
+  <article id="post-<?php the_ID(); ?>" <?php post_class('template-page'); ?>>
+    <?php
+    // Prepare hero data
+    $hero_args = array(
+      'hero_title' => get_the_title()
+    );
 
-						<?php the_title('<h1 class="template-page__title">', '</h1>'); ?>
+    // Add lead text as hero text if available
+    if (!empty($lead_paragraphs)) {
+      $hero_args['hero_text'] = implode("\n\n", $lead_paragraphs);
+    }
 
-						<?php if (!empty($lead_paragraphs)) : ?>
-							<?php foreach ($lead_paragraphs as $paragraph) : ?>
-								<p class="template-page__lead">
-									<?php echo esc_html($paragraph); ?>
-								</p>
-							<?php endforeach; ?>
-						<?php endif; ?>
-					</header>
+    get_template_part('template-parts/components/hero', null, $hero_args);
+    ?>
 
-					<section class="template-page__content">
-						<?php
-						the_content();
+    <div class="template-page__container">
+      <div class="template-page__row">
+        <div class="template-page__col">
 
-						wp_link_pages(
-							array(
-								'before' => '<div class="page-links">' . esc_html__('Pages:', 'aera'),
-								'after'  => '</div>',
-							)
-						);
-						?>
-					</section>
-				</div>
-			</div>
-		</div>
-	</article><!-- #post-<?php the_ID(); ?> -->
+          <section class="template-page__content">
+            <?php
+            the_content();
 
-	<?php
-	// If comments are open or we have at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) :
-		comments_template();
-	endif;
+            wp_link_pages(
+              array(
+                'before' => '<div class="page-links">' . esc_html__('Pages:', 'aera'),
+                'after'  => '</div>',
+              )
+            );
+            ?>
+          </section>
+        </div>
+      </div>
+    </div>
+  </article><!-- #post-<?php the_ID(); ?> -->
+
+<?php
+  // If comments are open or we have at least one comment, load up the comment template.
+  if (comments_open() || get_comments_number()) :
+    comments_template();
+  endif;
 
 endwhile; // End of the loop.
 
